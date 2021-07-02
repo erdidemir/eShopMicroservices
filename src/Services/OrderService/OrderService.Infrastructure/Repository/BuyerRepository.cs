91ed22f5c87e2810f6f493aa1a81a5a3e9355b94
Erdi Demir <erdi.demir@icloud.com>
2021-07-02 17:35:51 +0300

    Order DDD completed

---
+++ src/Services/OrderService/OrderService.Infrastructure/Repository/BuyerRepository.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.Application.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Repository
+{
+    public class BuyerRepository : IBuyerRepository
+    {
+        public Task<int> SaveChangesAsync()
+        {
+            return Task.FromResult(1);
+        }
+    }
+}
